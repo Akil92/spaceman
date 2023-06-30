1252b6a0a652382da4bcf0888bbcda23e380aa9f
Akil92 <akilmaynard@yahoo.com>
2023-06-30 15:12:14 -0400

    wordlength = dashes

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("JS works")
+
+const testWord = "example"
+
+console.log(testWord.length);
+
+const letterList = document.querySelector("section")
+
+for(let i = 0; i < testWord.length; i++) {
+ let letterSpace = document.createElement("div");
+ letterSpace.innerText = "_"
+ console.log(letterSpace)
+ letterList.appendChild(letterSpace)
+}
